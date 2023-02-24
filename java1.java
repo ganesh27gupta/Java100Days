12651e16674fe05179dc5ed9152ab6499d3b5342
ganesh27gupta <108972691+ganesh27gupta@users.noreply.github.com>
2023-02-24 19:46:10 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+class MyFirst 
+{
+    public static void main(String arg[])
+    {
+        System.out.println("HELLO WORLD DAY 1");
+    }
+}//Thank you for watching 
---
+++ java2.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+class keyRead
+{
+    public static void main(String[] args)
+     {
+         Scanner s = new Scanner(System.in);
+         String name;
+         System.out.print("May i know your Name:-");
+         name = s.nextInt();
+         System.out.println("Welcome Mr/Mrs "+name);
+        
+    }
+}//thank you for watching 
