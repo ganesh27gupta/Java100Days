69a9ebb695869621f6d6b87790b2f9bddd6b5618
ganesh27gupta <108972691+ganesh27gupta@users.noreply.github.com>
2023-02-24 19:46:39 +0530

    Add files via upload

---
+++ java3.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+public class java3
+{
+public static void main(String[] args)
+ {
+
+
+    Scanner s1 = new Scanner(System.in);
+     int x,y;
+    System.out.println("Enter 2 numbes :-");
+    x = s1.nextInt();
+    y = s1.nextInt();
+
+    int z = x + y ;
+    System.out.println("addition of 2 number is "+z);
+}
+} //thank you for watching
